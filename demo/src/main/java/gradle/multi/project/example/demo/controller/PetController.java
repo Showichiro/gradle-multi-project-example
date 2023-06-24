e266f839f5b413e398281e2b82fcd75d991c1de8
uchihori <show2305@gmail.com>
2023-06-24 17:01:23 +0900

    use buildSrc

---
+++ demo/src/main/java/gradle/multi/project/example/demo/controller/PetController.java
@@ -0,0 +1,10 @@
+package gradle.multi.project.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import openapi.generated.api.PetApi;
+
+@RestController
+public class PetController implements PetApi {
+    
+}
--- repository/src/main/java/gradle/multi/project/example/repository/TodoRepository.java
+++
@@ -1,6 +0,0 @@
-package gradle.multi.project.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
